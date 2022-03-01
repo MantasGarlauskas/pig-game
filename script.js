001466ea8cd4282402b209560452f90aa7a33132
Mantas Garlauskas <mantas.garlauskas@gmail.com>
2022-03-01 12:51:13 +0200

    players registration modal

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ const diceDOM = document.querySelector('.dice');
 const btnNewDOM = document.querySelector('.btn--new');
 const btnRollDOM = document.querySelector('.btn--roll');
 const btnHoldDOM = document.querySelector('.btn--hold');
-let scores, currentScore, activePlayer, playing;
+const modalDOM = document.querySelector('.modal');
+const closeModalDOM = document.querySelector('.close-modal');
+const overlayDOM = document.querySelector('.overlay');
+const startGameDOM = document.querySelector('.btn--start');
+const player1InputDOM = document.getElementById('player-1-name');
+const player2InputDOM = document.getElementById('player-2-name');
+const player1NameDOM = document.getElementById('name--0');
+const player2NameDOM = document.getElementById('name--1');
+const errorMsgDOM = document.querySelector('.error');
+let scores, currentScore, activePlayer, playing, winner;
 
 // Starting conditions
 const init = function() {
@@ -29,7 +38,6 @@ const init = function() {
     player0DOM.classList.add('player--active');
     player1DOM.classList.remove('player--active');
 };
-
 init();
 
 const switchPlayer = function() {
@@ -44,7 +52,6 @@ const switchPlayer = function() {
 btnRollDOM.addEventListener('click', () => {
     if (playing) {
         const dice = Math.trunc(Math.random() * 6) + 1;
-        console.log(dice);
         diceDOM.classList.remove('hidden');
         diceDOM.src = `./img/dice-${dice}.png`;
         if (dice !== 1) {
@@ -61,17 +68,49 @@ btnHoldDOM.addEventListener('click', () => {
     if (playing) {
         scores[activePlayer] += currentScore;
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-        if (scores[activePlayer] >= 50) {
+        if (scores[activePlayer] >= 10) {
             playing = false;
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
             diceDOM.classList.add('hidden');
+            winner += document.getElementById(`name--${activePlayer}`).textContent;
         } else {
             switchPlayer();
         }
     }
 
 });
+console.log(winner);
 
 //Reseting game functionality
-btnNewDOM.addEventListener('click', init);
+btnNewDOM.addEventListener('click', openModal);
+
+//open-close modal & start game button
+function openModal() {
+    modalDOM.classList.remove('hidden');
+    overlayDOM.classList.remove('hidden');
+}
+
+function closeModal() {
+    modalDOM.classList.add('hidden');
+    overlayDOM.classList.add('hidden');
+    errorMsgDOM.textContent = ''
+};
+
+function startGameModal() {
+    if (player1InputDOM.value && player2InputDOM.value) {
+        player1NameDOM.textContent = player1InputDOM.value;
+        player2NameDOM.textContent = player2InputDOM.value;
+        closeModal();
+        player1InputDOM.value = '';
+        player2InputDOM.value = '';
+    } else {
+        errorMsgDOM.textContent = `Please enter player names`;
+    }
+    init();
+};
+
+closeModalDOM.addEventListener('click', closeModal);
+startGameDOM.addEventListener('click', startGameModal);
+
+openModal();
